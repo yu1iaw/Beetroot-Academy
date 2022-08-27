5786ecc8e5eff3a44e9c8f475c7eed37a8fe1d6c
yu1ia <vasilenyuky@gmail.com>
2022-08-27 09:16:27 +0300

    semantic elem

---
+++ 2/src/script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const code = document.querySelectorAll('.code');
+
+code[0].textContent = `
+    <section>
+        <h1>WWF</h1>
+        <p>The World Wide Fund for Nature (WWF) is...</p>
+    </section>
+`;
+code[1].textContent = `
+    <article>
+        <h1>What does WWF do?</h1>
+        <p>
+            WWF's mission is to stop the degradation of our planet's natural environment, 
+            and build a future in which humans live in garmony with nature.
+        </p>
+    </article>
+`;
+code[2].textContent = `
+    <article>
+        <header>
+            <h1>What does WWF do?</h1>
+            <p>WWF's mission:</p>
+        </header>
+        <p>
+            WWF's mission is to stop the degradation of our planet's natural environment, 
+            and build a future in which humans live in garmony with nature.
+        </p>
+    </article>
+`;
+code[3].textContent = `
+    <footer>
+        <p>
+            Posted by: Hege Refsnes
+        </p>
+        <p>
+            Contact information: <a href="mailto:someone@html5css.com">someone@html5css.com</a>.
+        </p>
+    </footer>
+`;
+
+
